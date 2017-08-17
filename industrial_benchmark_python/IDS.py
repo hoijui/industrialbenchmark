03ea59ef9369bd2f89f9e081d10385763054797a
hoijui <hoijui.quaero@gmail.com>
2017-08-17 09:49:12 +0200

    python: IDS: white-space fixes [minor]

--- industrial_benchmark_python/IDS.py
+++ industrial_benchmark_python/IDS.py
@@ -33,7 +33,7 @@ from collections import OrderedDict
 class IDS(object):
     '''
     Lightweight python implementation of the industrial benchmark
-    Uses the same standard settings as in src/main/ressources/simTest.properties 
+    Uses the same standard settings as in src/main/ressources/simTest.properties
     of java implementation
     '''
 
@@ -47,11 +47,11 @@ class IDS(object):
         will make the system more non-stationary.
         '''
 
-        
+
         self.maxRequiredStep = np.sin(15./180.*np.pi);
         self.gsBound = 1.5
         self.gsSetPointDependency = 0.02
-	self.gsScale = 2.*self.gsBound + 100.*self.gsSetPointDependency
+        self.gsScale = 2.*self.gsBound + 100.*self.gsSetPointDependency
 
         self.CRD = 3.
         self.CRE = 1
@@ -62,18 +62,18 @@ class IDS(object):
         self.gsEnvironment = GoldstoneEnvironment(24, self.maxRequiredStep, self.maxRequiredStep/2.0);
 
         self.state = OrderedDict()
-        self.state['o'] = np.zeros(10) #  operational cost buffer 
+        self.state['o'] = np.zeros(10) #  operational cost buffer
         self.state['coc'] = 0 # current operational cost
         self.state['hg'] = 0. # hidden gain
         self.state['hv'] = 0. # hidden velocity
         self.state['hs'] = 0. # hidden shift
-        self.state['gs_domain'] = self.gsEnvironment._dynamics._domain.value 
+        self.state['gs_domain'] = self.gsEnvironment._dynamics._domain.value
         self.state['gs_sys_response'] = self.gsEnvironment._dynamics._system_response.value
         self.state['gs_phi_idx'] = self.gsEnvironment._dynamics._Phi_idx
         self.state['ge'] = 0. # effective gain beta
         self.state['ve'] = 0. # effective velocity alpha
         self.state['MC'] = 0.
-        self.state['c'] = 0. # consumption 
+        self.state['c'] = 0. # consumption
         self.state['p'] = p  # SetPoint
         self.state['v'] = 50. # Velocity
         self.state['g'] = 50.  # Gain
@@ -110,15 +110,15 @@ class IDS(object):
         self.state['v'] = np.clip(self.state['v'] + delta[0],0.,100.)
         self.state['g'] = np.clip(self.state['g'] + 10*delta[1],0.,100.)
         self.state['s'] = np.clip(self.state['s'] + ((self.maxRequiredStep/0.9)*100./self.gsScale)*delta[2],0.,100.)
-	self.state['hs'] = np.clip(self.gsScale*self.state['s']/100. - self.gsSetPointDependency*self.state['p'] - self.gsBound,-self.gsBound,self.gsBound) # hidden/effective shift
+        self.state['hs'] = np.clip(self.gsScale*self.state['s']/100. - self.gsSetPointDependency*self.state['p'] - self.gsBound,-self.gsBound,self.gsBound) # hidden/effective shift
 
 
     def updateFatigue(self):
         expLambda = 0.1 # => scale = 1/lambda
         actionTolerance = 0.05
-        fatigueAmplification = 1.1    
+        fatigueAmplification = 1.1
         fatigueAmplificationMax = 5.0
-        fatigueAmplificationStart = 1.2 
+        fatigueAmplificationStart = 1.2
 
         dyn = 0.0
         v = self.state['v']
@@ -139,10 +139,10 @@ class IDS(object):
         noise_e_v = np.random.exponential(expLambda);
         noise_u_g = np.random.rand();
         noise_u_v = np.random.rand();
-        
+
         noise_b_g = np.float(np.random.binomial(1, np.clip(ge,0.001, 0.999)))
         noise_b_v = np.float(np.random.binomial(1, np.clip(ve,0.001, 0.999)))
-        
+
 
         ng = 2.0 * (1.0/(1.0+np.exp(-noise_e_g)) - 0.5);
         nv = 2.0 * (1.0/(1.0+np.exp(-noise_e_v)) - 0.5);
@@ -169,7 +169,7 @@ class IDS(object):
             hg = ge
 
 
-        if np.maximum(hv,hg) == fatigueAmplificationMax: 
+        if np.maximum(hv,hg) == fatigueAmplificationMax:
             alpha = 1.0 / (1.0 + np.exp(-4.0*np.random.normal(0.6,0.1)))
         else:
             alpha = np.maximum(nv,ng)
@@ -190,7 +190,7 @@ class IDS(object):
         velocity = self.state['v']
         setpoint = self.state['p']
 
-	costs = CostSetPoint * setpoint + CostGain * gain + CostVelocity * velocity;
+        costs = CostSetPoint * setpoint + CostGain * gain + CostVelocity * velocity;
         o = np.exp(costs / 100.)
         self.state['coc'] = o
 
@@ -213,7 +213,7 @@ class IDS(object):
 
         reward = -self.gsEnvironment.state_transition(hs)
         self.state['MC'] = reward
-        self.state['gs_domain'] = self.gsEnvironment._dynamics._domain.value 
+        self.state['gs_domain'] = self.gsEnvironment._dynamics._domain.value
         self.state['gs_sys_response'] = self.gsEnvironment._dynamics._system_response.value
         self.state['gs_phi_idx'] = self.gsEnvironment._dynamics._Phi_idx
 
